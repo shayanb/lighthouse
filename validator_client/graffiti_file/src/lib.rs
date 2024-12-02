348823caab69ad98db0247aa6e8420824527ac56
Shayan Eskandari <shayanbahal@gmail.com>
2024-12-01 20:28:30 -0500

    cargo fmt

--- validator_client/graffiti_file/src/lib.rs
+++ validator_client/graffiti_file/src/lib.rs
@@ -137,7 +137,7 @@ mod tests {
     const CUSTOM_GRAFFITI2: &str = "graffitiwall:720:641:#ffff00";
     const EMPTY_GRAFFITI: &str = "";
     // Newline test cases
-    const CUSTOM_GRAFFITI4: &str = "newlines-tests"; 
+    const CUSTOM_GRAFFITI4: &str = "newlines-tests";
 
     const PK1: &str = "0x800012708dc03f611751aad7a43a082142832b5c1aceed07ff9b543cf836381861352aa923c70eeb02018b638aa306aa";
     const PK2: &str = "0x80001866ce324de7d80ec73be15e2d064dcf121adf1b34a0d679f2b9ecbab40ce021e03bb877e1a2fe72eaaf475e6e21";
@@ -194,7 +194,7 @@ mod tests {
             .unwrap();
         graffiti_file.write_all(b"\n").unwrap();
         graffiti_file.write_all(b"   \n").unwrap();
-        
+
         graffiti_file.flush().unwrap();
         file_name
     }
